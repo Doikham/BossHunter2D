9dd241bcc1fc8d237f9d0a2752f3867ef8c58c99
big <big_sethawit@hotmail.com>
2019-07-17 21:25:49 +0700

    Initial commit

---
+++ src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class MainFrame
+{
+    private JPanel          contentpane;
+    
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
