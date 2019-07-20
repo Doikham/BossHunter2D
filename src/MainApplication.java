409b6cc94af224fa901abc7a7018e2de52729ea8
big <big_sethawit@hotmail.com>
2019-07-20 17:19:17 +0700

    L

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MainApplication extends JFrame
-{
+public class MainApplication extends JFrame implements KeyListener {
     private JPanel           contentpane;
     private JLabel           drawpane;
     //private JButton          offButton, rideButton;
@@ -55,8 +58,23 @@ public class MainApplication extends JFrame
 
         HeroLabel = new JLabel(HeroImg);
         HeroLabel.setBounds(HeroCurX, HeroCurY, HeroWidth, HeroHeight);
-        drawpane.add(HeroLabel);
+        HeroLabel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
 
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        drawpane.add(HeroLabel);
         BossLabel = new JLabel(BossImg);
         BossLabel.setBounds(BossCurX, BossCurY, BossWidth, BossHeight);
         drawpane.add(BossLabel);
@@ -66,7 +84,7 @@ public class MainApplication extends JFrame
         control.setBackground(new Color(102, 204, 0));
         contentpane.add(control, BorderLayout.NORTH);
         contentpane.add(drawpane, BorderLayout.CENTER);
-
+        addKeyListener(this);
         validate();
     }
 
@@ -108,7 +126,128 @@ public class MainApplication extends JFrame
         BossThread.start();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W)
+        {
+            if (HeroCurY > 0) HeroCurY -= 20;
+            else HeroCurY = 0;
+            setLocation(HeroCurX, HeroCurY);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S)
+        {
+            if (HeroCurY < 510 - frameHeight) HeroCurY += 20;
+            else HeroCurY = 510 - frameHeight;
+            setLocation(HeroCurX, HeroCurY);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A)
+        {
+            if (HeroCurX < 20) HeroCurX = 635;
+            else HeroCurX -= 20;
+            setLocation(HeroCurX, HeroCurY);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            if (HeroCurX > 630) HeroCurX = 5;
+            else HeroCurX += 20;
+            setLocation(HeroCurX, HeroCurY);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
+
+//class MyLabel extends JLabel implements MouseListener
+//{
+//    private int width = 150, height = 200;      // adjust label size as you want
+//    private MyImageIcon   icon1, icon2;
+//    private int           curX, curY;
+//    private boolean       horizontalMove, verticalMove;
+//
+//    public MyLabel(String file1, String file2)
+//    {
+//        icon1 = new MyImageIcon(file1).resize(width, height);
+//        icon2 = new MyImageIcon(file2).resize(width, height);
+//        setHorizontalAlignment(JLabel.CENTER);
+//        setIcon(icon1);
+//        addMouseListener(this);
+//    }
+//
+//    public void setMoveConditions(int x, int y, boolean h, boolean v)
+//    {
+//        curX = x; curY = y;
+//        setBounds(curX, curY, width, height);
+//        horizontalMove = h; verticalMove = v;
+//    }
+//
+//    // add your own code
+//    public void moveUp()    {
+//        if(verticalMove) {
+//            if (curY > 0) curY -= 20;
+//            else curY = 0;
+//            setLocation(curX, curY);
+//        }
+//    }
+//    public void moveDown()  {
+//        if(verticalMove) {
+//            if (curY < 510 - height) curY += 20;
+//            else curY = 510 - height;
+//            setLocation(curX, curY);
+//        }
+//    }
+//    public void moveLeft()  {
+//        if(horizontalMove) {
+//            if (curX < 20) curX = 635;
+//            else curX -= 20;
+//            setLocation(curX, curY);
+//        }
+//    }
+//    public void moveRight() {
+//        if(horizontalMove) {
+//            if (curX > 630) curX = 5;
+//            else curX += 20;
+//            setLocation(curX, curY);
+//        }
+//    }
+//
+//
+//    @Override
+//    public void mouseClicked(MouseEvent e) {
+//        if(e.getButton() == MouseEvent.BUTTON1) {
+//            setIcon(icon2);
+//        }
+//        if(e.getButton() == MouseEvent.BUTTON3) {
+//            setIcon(icon1);
+//        }
+//    }
+//
+//    @Override
+//    public void mousePressed(MouseEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void mouseReleased(MouseEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void mouseEntered(MouseEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void mouseExited(MouseEvent e) {
+//
+//    }
+//}
 
 class MyImageIcon extends ImageIcon
 {
